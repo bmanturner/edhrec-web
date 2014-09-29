c2efb12f5e7d07cfc1d1b35d7f895c262348e30e
bfturner <brendanfturner@gmail.com>
2014-09-29 12:56:47 -0500

    Split noncreatures

--- fetch_results.py
+++ fetch_results.py
@@ -12,13 +12,13 @@ import random
 import urllib2
 import HTMLParser
 import cgi
-import cgitb
-cgitb.enable()
+
+print "Content-type:text/html\n\r\n\r"
+
 
 def linkify(cn):
     return '<a href="http://gatherer.wizards.com/Handlers/Image.ashx?name=%s&type=card&.jpg" target="_blank">%s</a>' % (cn, core.cap_cardname(cn))
 
-print "Content-type:text/html\r\n\r\n"
 
 form = cgi.FieldStorage()
 url = form['inputurl'].value
@@ -29,6 +29,7 @@ url = tappedout.URL_PATTERN.match(url)
  
 
 if url is None:
+    
     print '<h2>That Uril wasn\'t recognized, sorry...</h2>' #badum tss
 else:
     url = str(url.group(1))  
@@ -38,7 +39,9 @@ else:
     newrecs, outrecs = core.recommend(deck)
     lands = []
     creatures =[]
-    noncreatures = []  
+    enchantments = []
+    artifacts = []
+    spells = []
 
     for card, score in newrecs:
         # filter out basic lands from being recommendations
@@ -62,12 +65,16 @@ else:
             creatures.append((card, score))
         elif 'Land' in types:
             lands.append((card, score))
+        elif 'Enchantment' in types:
+            enchantments.append((card,score))
+        elif 'Artifact' in types:
+            artifacts.append((card,score))
         else:
-            noncreatures.append((card, score))
+            spells.append((card, score))
     out_str = []
 
-    out_str.append('<table class="table table-striped table-condensed table-bordered"><tr><th>Creatures</th><th>Non-creatures</th><th>Lands</th><th>Unique in your deck</th></tr>')
-    for i in range(16):
+    out_str.append('<table class="table table-striped table-condensed table-bordered"><tr><th>Creatures</th><th>Spells</th><th>Enchantments</th><th>Artifacts</th><th>Lands</th></tr>')
+    for i in range(20):
 
         try:
             c = '<span class="badge">%d</span> %s ' % (creatures[i][1], linkify(creatures[i][0]))
@@ -75,26 +82,36 @@ else:
             c = ' '
 
         try:
-            n = '<span class="badge">%d</span> %s ' % (noncreatures[i][1], linkify(noncreatures[i][0]))
+            s = '<span class="badge">%d</span> %s ' % (spells[i][1], linkify(spells[i][0]))
         except IndexError:
-            n = ' '
+            s = ' '
+
+        try:
+            e = '<span class="badge">%d</span> %s ' % (enchantments[i][1], linkify(enchantments[i][0]))
+        except IndexError:
+            e = ' '
+
+        try:
+            a = '<span class="badge">%d</span> %s ' % (artifacts[i][1], linkify(artifacts[i][0]))
+        except IndexError:
+            a = ' '
 
         try:
             l = '<span class="badge">%d</span> %s ' % (lands[i][1], linkify(lands[i][0]))
         except IndexError:
             l = ' '
 
-        try:
-            u = '%s ' % linkify(outrecs[i][0])
-        except IndexError:
-            u = ' '
+        #try:
+        #    u = '%s ' % linkify(outrecs[i][0])
+        #except IndexError:
+        #    u = ' '
 
-        if len(c + n + l) == 3:
+        if len(c + s + e + a + l) == 5:
            break
 
-        out_str.append('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>' % (c, n , l, u))
+        out_str.append('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>' % (c, s , e, a, l))
     out_str.append('</table>')
     core.add_deck(deck)
     for line in out_str:
-    	print '<p>%s</p>' % line
+        print '<p>%s</p>' % line
 
